2975590694925ccaf30e9eb54fe43a13a930e801
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-11-03 22:18:25 +0300

    fix: edit result card fields

--- src/components/ResultsCard.tsx
+++ src/components/ResultsCard.tsx
@@ -8,19 +8,20 @@ export default function ResultsCard(props: ResultCardProps) {
   return (
     <div className="bg-slate-500 max-w-xs p-4 rounded">
       <a
-        href={typeof props.item.imdbUrl === 'string' ? props.item.imdbUrl : ''}
+        href={
+          typeof props.item.promoUrl === 'string' ? props.item.promoUrl : ''
+        }
         className="underline my-1 hover:text-orange-300 transition"
       >
-        <span className="">Title: </span>
         <span className="font-bold">{props.item.titleOriginal}</span>
       </a>
       <div>
-        <span className="">Country: </span>
-        <span className="font-bold">{props.item.country}</span>
+        <span className="">Total seasons: </span>
+        <span className="font-bold">{props.item.totalSeasons}</span>
       </div>
       <div>
-        <span className="">IMDB Rating: </span>
-        <span className="font-bold">{props.item.imdbRating}</span>
+        <span className="">Rating: </span>
+        <span className="font-bold">{props.item.rating}</span>
       </div>
     </div>
   );
