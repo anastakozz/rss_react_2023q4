20b71a08f7e7c5d2da46ef5635fc0f22a9d9c2db
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-02 21:30:55 +0300

    feat: add picture loading in Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,10 +6,22 @@ type CardProps = {
 };
 
 export default function Card({ item, isLast }: CardProps) {
-  
   return (
-    <div className={isLast ? 'bg-orange-400' : ' bg-blue-500'}>
-      {item.name} {item.age} {item.email}
+    <div className={(isLast ? ' border-orange-400' : ' border-white/50') + ' w-1/3 border border-4'}>
+      <div className="p-4">
+        <ul className="mb-4">
+          <li>Name : {item.name}</li>
+          <li>Age: {item.age}</li>
+          <li>Email : {item.email}</li>
+          <li>Gender: {item.gender}</li>
+          <li>Country: {item.country}</li>
+          <li>Password: {item.password}</li>
+        </ul>
+
+        <img
+          src={typeof item.picture === 'string' ? item.picture : ''}
+        ></img>
+      </div>
     </div>
   );
 }
