61e7f91d0da46994716e7478e10f2d9a20688f3e
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-12-03 16:12:53 +0300

    feat: add RHF select typing

--- src/components/inputComponents/GenderSelect.tsx
+++ src/components/inputComponents/GenderSelect.tsx
@@ -1,21 +1,40 @@
 import React, { ReactNode, LegacyRef } from 'react';
+import { ChangeHandler, UseFormRegister } from 'react-hook-form';
 
 interface GenderSelectProps {
   children: ReactNode;
   title: string;
+  name?: string;
+  onBlur?: () => void | ChangeHandler;
+  onChange?: () => void | ChangeHandler;
 }
+
+interface IFormInput {
+  name: string;
+  age: number;
+  email: string;
+  gender: string;
+  password: string;
+  repeatedPassword?: string | undefined;
+  picture: File;
+  terms?: boolean | undefined;
+}
+
 
 const GenderSelect = React.forwardRef(
   (
-    { children, title }: GenderSelectProps,
+    { children, title, name, onBlur, onChange }: GenderSelectProps,
     ref: LegacyRef<HTMLSelectElement> | undefined
   ) => {
     return (
       <label className="min-w-full">
-        <div className="max-w-contain relative block">{title}</div>
+        {title}
         <select
-          className="h-[24px] w-full rounded px-4 font-normal text-black"
+          name={name}
           ref={ref}
+          onChange={onChange}
+          onBlur={onBlur}
+          className="h-[24px] w-full rounded px-4 font-normal text-black"
         >
           <option value="not selected">Not selected</option>
           <option value="male">Male</option>
@@ -27,5 +46,29 @@ const GenderSelect = React.forwardRef(
     );
   }
 );
+
+export const HookGenderSelect = React.forwardRef<
+  HTMLSelectElement,
+  { title: string; children?: ReactNode } & ReturnType<
+    UseFormRegister<IFormInput>
+  >
+>(({ onChange, onBlur, name, title, children }, ref) => (
+  <label className="min-w-full">
+    {title}
+    <select
+      name={name}
+      ref={ref}
+      onChange={onChange}
+      onBlur={onBlur}
+      className="h-[24px] w-full rounded px-4 font-normal text-black"
+    >
+      <option value="not selected">Not selected</option>
+      <option value="male">Male</option>
+      <option value="female">Female</option>
+      <option value="other">Other</option>
+    </select>
+    {children}
+  </label>
+));
 
 export default GenderSelect;
